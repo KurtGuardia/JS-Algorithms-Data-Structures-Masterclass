d67af5a8f0aae9a51cee78e1f78ec8cdb5252609
Kurt Guardia <kurtguardia@gmail.com>
2022-03-09 14:26:50 +0100

    multipointers arr

--- multiple pointers.js
+++ multiple pointers.js
@@ -7,20 +7,33 @@
 //   console.log(sum);
 // };
 
-const countUniqueValues = (arr) => {
-  if (arr.length === 0) return;
-  let i = 0;
-  for (let j = 1; j < arr.length; j++) {
-    if (arr[i] !== arr[j]) {
-      i++;
-      arr[i] = arr[j];
-    }
+// const countUniqueValues = (arr) => {
+//   if (arr.length === 0) return;
+//   let i = 0;
+//   for (let j = 1; j < arr.length; j++) {
+//     if (arr[i] !== arr[j]) {
+//       i++;
+//       arr[i] = arr[j];
+//     }
+//   }
+//   // i plus one is the correct length of the unique values inside the modified array
+//   return console.log(i + 1);
+// };
+
+// countUniqueValues([1, 1, 1, 2]);
+// countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]);
+// countUniqueValues([]);
+// countUniqueValues([-2, -1, -1, 0, 1]);
+
+// CHALLENGE
+
+const areThereDuplicates = (...arg) => {
+  const orderedArr = arg.sort();
+  for (let i = 0; i < orderedArr.length; i++) {
+    const el = orderedArr[i];
+    if (el === orderedArr[i + 1]) return true;
   }
-  // i plus one is the correct length of the unique values inside the modified array
-  return console.log(i + 1);
+  return false;
 };
 
-countUniqueValues([1, 1, 1, 2]);
-countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]);
-countUniqueValues([]);
-countUniqueValues([-2, -1, -1, 0, 1]);
+console.log(areThereDuplicates('1', '2', '3', '2'));
